03fa823e3c23592cc799627dfe9b0fc23e5ec4a1
Maciej Znalezniak <maciej.znalezniak@gmail.com>
2023-11-27 05:38:05 +0100

    fix:bucket2

--- aws-app/main.tf
+++ aws-app/main.tf
@@ -1,12 +1,13 @@
 resource "aws_s3_bucket" "bucket" {
-    bucket = "frontend-app"
+    bucket = "frontend-app2"
   }
 
   resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = "frontend-app"
+    bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = false
+    block_public_acls       = true
+    block_public_policy     = false
+    ignore_public_acls      = true
+    restrict_public_buckets = false
+
 }
